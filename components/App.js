9686ddc682c7d2ad0e194088c6ffe961109a7cb5
tom-aglow <tom@ohhhh.me>
2018-02-20 15:02:46 -0800

    hello native world

---
+++ components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import * as firebase from 'firebase';
+import firebaseConfig from '../credentials.json';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.instructions}>{'Hello world'}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+});
