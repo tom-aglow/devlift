1e7eca8c130ae2221a1b8509d28a30861466793d
tom-aglow <tom@ohhhh.me>
2018-02-20 15:17:47 -0800

    add eslint and prettier to the project

--- components/App.js
+++ components/App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import * as firebase from 'firebase';
-import firebaseConfig from '../credentials.json';
+import * as firebase from "firebase";
+import firebaseConfig from "../credentials.json";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-type Props = {};
-export default class App extends Component<Props> {
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.instructions}>{'Hello world'}</Text>
+        <Text style={styles.instructions}>{"Hello world"}</Text>
       </View>
     );
   }
@@ -20,13 +19,15 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5
   }
 });
+
+export default App;
