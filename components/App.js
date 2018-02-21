2c6e94c67cd7fe813d7c328dc90c2a9eb6fe3c21
tom-aglow <tom@ohhhh.me>
2018-02-20 17:02:43 -0800

    make simple boiler plate for NewTodo component and state inside App component

--- components/App.js
+++ components/App.js
@@ -1,16 +1,49 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import NewTodo from "./NewTodo.js";
 import * as firebase from "firebase";
 import firebaseConfig from "../credentials.json";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
+  state = {
+    inputValue: "",
+    items: []
+  };
+
+  handleAddItem = () => {
+    if (!this.state.inputValue) return null;
+
+    const newItems = [
+      ...this.state.items,
+      {
+        id: Date.now(),
+        text: this.state.inputValue,
+        completed: false
+      }
+    ];
+
+    this.setState({
+      items: newItems,
+      inputValue: ""
+    });
+  };
+
+  handleInputChange = inputValue => {
+    this.setState({ inputValue });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.instructions}>{"Hello world"}</Text>
+        <Text>{"Hello world"}</Text>
+        <NewTodo
+          inputValue={this.state.inputValue}
+          onAddItem={this.handleAddItem}
+          onChange={this.handleInputChange}
+        />
       </View>
     );
   }
@@ -22,11 +55,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F5FCFF"
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
   }
 });
 
